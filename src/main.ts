28146debb71fc1da90ee7e875aed2624ce81888b
Peter Leibiger <kuhnroyal@gmail.com>
2024-03-20 17:43:44 +0100

    Support pull_request_target event
    The pull_request_target is used for pull requests originating from forks with read only permissions by default.

--- src/main.ts
+++ src/main.ts
@@ -30,7 +30,8 @@ async function run(): Promise<void> {
     core.debug(`filename: ${filename}`)
 
     switch (process.env.GITHUB_EVENT_NAME) {
-      case 'pull_request': {
+      case 'pull_request': 
+      case 'pull_request_target': {
         const {GITHUB_BASE_REF = ''} = process.env
         core.debug(`GITHUB_BASE_REF: ${GITHUB_BASE_REF}`)
         const artifactPath = await downloadArtifacts(GITHUB_BASE_REF)
